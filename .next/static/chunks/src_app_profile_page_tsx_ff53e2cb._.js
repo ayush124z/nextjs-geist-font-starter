a368b35ea82baa48f3d1a0411e22dc797f23e167
ayush124z <ayushmishraji7905@gmail.com>
2025-08-24 11:03:03 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_ff53e2cb._.js 1756013582660

---
+++ .next/static/chunks/src_app_profile_page_tsx_ff53e2cb._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_62fa7e36._.js",
+  "static/chunks/node_modules_83368669._.js"
+],
+    source: "dynamic"
+});
